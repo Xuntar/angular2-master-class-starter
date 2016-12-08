0c9aea743bfbd64dda673839dfc34c94d74b4fc7
Bruneel Frederik <frederik.bruneel@cegeka.be>
2016-12-08 11:40:59 +0100

    Exercise 3: Create list and trackBy

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Contact } from './models/contact'
+import { Contact } from './models/contact';
+import { CONTACT_DATA } from './data/contact-data';
 
 // Every component has a view
 @Component({
@@ -9,19 +10,30 @@ import { Contact } from './models/contact'
   styleUrls: ['contacts.component.css']
 })
 export class ContactsAppComponent {
-  title = 'Angular 2 Master Class setup works, I think!';
-  contact: Contact = {
-    id: 7,
-    name: 'Diana Ellis',
-    email: '',
-    phone: '',
-    birthday: '',
-    image: '/assets/images/6.jpg',
-    address: {
-      street: '6554 park lane',
-      zip: '43378',
-      city: 'Rush',
-      country: 'United States'
-    }
+  // title = 'Angular 2 Master Class setup works, I think!';
+  // contact: Contact = {
+  //   id: 7,
+  //   name: 'Diana Ellis',
+  //   email: '',
+  //   phone: '',
+  //   birthday: '',
+  //   image: '/assets/images/6.jpg',
+  //   address: {
+  //     street: '6554 park lane',
+  //     zip: '43378',
+  //     city: 'Rush',
+  //     country: 'United States'
+  //   }
+  // }
+  contacts: Contact[] = CONTACT_DATA;
+
+  trackByContactId(index, contact){
+    console.log('Tracking by Id: ' + contact.id)
+    return contact.id;
+  };
+  
+  constructor(){
+    //this.contacts = CONTACT_DATA;
+    this.contacts[0].name = 'test';
   }
 }
